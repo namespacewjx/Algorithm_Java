640a8ff5ed69f2e9e139067c93cd64cfb521a67b
WuJunxian <wu812730157@gmail.com>
2017-03-22 11:29:37 +0800

    Add a stack implementation of postfix calculation

--- src/io/wicp/namespacewjx/algorithm/PostfixCalculate.java
+++ src/io/wicp/namespacewjx/algorithm/PostfixCalculate.java
@@ -1,15 +1,67 @@
 package io.wicp.namespacewjx.algorithm;
 
+import java.util.Stack;
+
 /**
  * Created by 吴俊贤 on 2017/3/21.
  */
 public class PostfixCalculate {
-	public static int calculate(String postfix) {
-		return getTree(postfix).getValue();
+	public static void main(String[] args) {
+		System.out.println(calculate("53*21*+"));
 	}
 
-	public static int cur = 0;//用来记录当前所指向的字符，用于创建结点的
-	public static Node getTree(String tree) {
+	public static int calculate(String postfix) {
+		return stackCal(postfix);
+	}
+
+	/**
+	 * 提供用栈来实现的算法，返回-1则代表字符串有错
+	 */
+	private static int stackCal(String postfix) {
+		Stack<Integer> stack = new Stack<>();
+		
+		for (int i = 0; i < postfix.length(); i++) {
+			char c = postfix.charAt(i);
+
+			if (c >= '0' && c <= '9') {
+				stack.push(c - '0');
+			} else if (c >= 'A' && c <= '9') {
+				stack.push(c - 'A' + 10);
+			} else {
+				int result;
+
+				//此时是运算符字符，如果栈里面没有两个元素，
+				// 则这个字符串是有问题的，有一个中间结点度为1，不是有效的树
+				if (stack.size() < 2)
+					return -1;
+
+				//注意，栈顶元素是右子树的结果，因此不能连续pop两次来算，必须先存着
+				int a = stack.pop();
+
+				if (c == '+') {
+					result = stack.pop() + a;
+				} else if (c == '-') {
+					result = stack.pop() - a;
+				} else if (c == '*') {
+					result = stack.pop() * a;
+				} else
+					return -1;//不是有效的算术符号的时候返回-1
+				stack.push(result);
+			}
+		}
+
+		//若此时栈还有很多个元素，也是有问题的字符串
+		if (stack.size() != 1)
+			return -1;
+
+		return stack.pop();
+	}
+
+	/**
+	 * 下面是建树的实现
+	 */
+	private static int cur = 0;//用来记录当前所指向的字符，用于创建结点的
+	private static Node getTree(String tree) {
 		if (cur < 0) {
 			return null;
 		}
@@ -97,4 +149,6 @@ public class PostfixCalculate {
 			return value;
 		}
 	}
+
+
 }
